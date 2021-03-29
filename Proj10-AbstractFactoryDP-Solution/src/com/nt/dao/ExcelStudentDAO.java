d442f70c9f2a8f5f79644f9a800ccce78a3858cf
srika <srika@192.168.0.4>
2021-03-29 23:21:07 +0530

    templeteDP

---
+++ Proj10-AbstractFactoryDP-Solution/src/com/nt/dao/ExcelStudentDAO.java
@@ -0,0 +1,10 @@
+package com.nt.dao;
+
+public class ExcelStudentDAO implements DAO {
+
+	@Override
+	public void insert() {
+		System.out.println("ExcelStudentDAO.inserting student info Excel Sheet");
+	}
+
+}
