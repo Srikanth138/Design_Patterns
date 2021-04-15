d98458e4c3b8de4d73fe1049fe6ce70421f24e2a
srika <srika@192.168.0.4>
2021-04-15 23:07:17 +0530

    ProxyDP

--- Proj19-ProxyDP-Solution/src/com/nt/test/Customer1.java
+++ Proj19-ProxyDP-Solution/src/com/nt/test/Customer1.java
@@ -1,11 +1,17 @@
 package com.nt.test;
 
+import com.nt.component.IBankService;
 import com.nt.factory.BankServiceFactory;
 
 public class Customer1 {
 	public static void main(String[] args) {
 		System.out.println(BankServiceFactory.getInstance(true).withdraw(1256, 5000));
-//		System.out.println(BankServiceFactory.getInstance(false).withdraw(1256, 2000));
+		
+		System.out.println("--------------------");
+		IBankService service = BankServiceFactory.getInstance(true);
+		System.out.println(service.withdraw(10001, 30000));
+		System.out.println("=========================");
+		System.out.println(service.withdraw(10001, 3000));
 	}
 
 }
