f94db536559ac43f019d1617b6c35270c9b1f018
srika <srika@192.168.0.6>
2021-04-16 21:59:18 +0530

    ProxyDP

---
+++ Proj20-ProxyDP-Solution-CGLIB/src/com/nt/component/BankServiceImpl.java
@@ -0,0 +1,10 @@
+package com.nt.component;
+
+public class BankServiceImpl {
+
+	
+	public String withdraw(long acno, double amount) {
+		return "withdrawing amount ::" + amount + "from account number ::" + acno;
+	}
+
+}
---
+++ Proj20-ProxyDP-Solution-CGLIB/src/com/nt/factory/BankServiceFactory.java
@@ -0,0 +1,18 @@
+package com.nt.factory;
+
+import com.nt.component.BankServiceImpl;
+import com.nt.proxy.BankProxyHelper;
+
+import net.sf.cglib.proxy.Enhancer;
+
+public class BankServiceFactory {
+	public static BankServiceImpl getInstance(boolean demonitisation) {
+		BankServiceImpl service = null;
+		if (demonitisation)
+			service = (BankServiceImpl) Enhancer.create(BankServiceImpl.class, new BankProxyHelper()); 
+		// generates and gives InMemory proxy class obj
+		else
+			service = new BankServiceImpl(); // real object
+		return service;
+	}// method
+}// class
